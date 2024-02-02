5c40d8a242f51a423595223e44ca5fe3022fdb8b
Sarvesh-Work <sarveshmunde10@gmail.com>
2024-02-02 11:30:29 +0530

    user and video models added

---
+++ src/models/user.models.js
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save",async function(next){
+  if(this.isModified("password")) return next()
+
+   this.password=bcrypt.hash(this.password,10)
+   next()
+})
+
+userSchema.method.isPasswordCorrect=async function (password)
+{
+  return await bcrypt.compare(password,this.password)
+}
+
+export const user = mongoose.model("User", userSchema);
